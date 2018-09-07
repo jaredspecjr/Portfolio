b8955cbed20a08379e31f987695b2efc6bf3ad5b
jared <jaredspecjr@gmail.com>
2018-09-07 15:59:30 -0700

    add other pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,39 @@
 $("#aboutButton").click(function(){
   $(".about").toggleClass('aboutClicked');
-  $(".hidden").fadeIn(1000);
+  $(".hiddenAbout").fadeIn(1000);
 });
 
 $("#aboutBack").click(function(){
   $(".about").toggleClass('aboutClicked');
-  $(".hidden").hide();
+  $(".hiddenAbout").hide();
+});
+
+$("#skillsButton").click(function(){
+  $(".skills").toggleClass('skillsClicked');
+  $(".hiddenSkills").fadeIn(1000);
+});
+
+$("#skillsBack").click(function(){
+  $(".skills").toggleClass('skillsClicked');
+  $(".hiddenSkills").hide();
+});
+
+$("#projectsButton").click(function(){
+  $(".projects").toggleClass('projectsClicked');
+  $(".hiddenProjects").fadeIn(1000);
+});
+
+$("#projectsBack").click(function(){
+  $(".projects").toggleClass('projectsClicked');
+  $(".hiddenProjects").hide();
+});
+
+$("#contactButton").click(function(){
+  $(".contact").toggleClass('contactClicked');
+  $(".hiddenContact").fadeIn(1000);
+});
+
+$("#contactBack").click(function(){
+  $(".contact").toggleClass('contactClicked');
+  $(".hiddenContact").hide();
 });
