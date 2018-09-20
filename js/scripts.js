cce39be96ccfc126764764d8f97ed0098ecb96f3
jared <jaredspecjr@gmail.com>
2018-09-20 12:34:43 -0700

    change layout and animations of each project on the projects page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-  setTimeout(function(){
-    $(".introHide").fadeOut(1600);
-  },5000);
-});
+// $(document).ready(function(){
+//   setTimeout(function(){
+//     $(".introHide").fadeOut(1600);
+//   },5000);
+// });
 
 
 $("#kotaku").click(function(){
